1dd3482008b362390c5c9954746b2aa705d19b49
Andrew Kochkin <kochkin_9@mail.ru>
2026-01-21 21:59:29 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import logo from './assets/img-react.jpg'
+
+const App = () => {
+  return <div>
+    <h1>Привет, мир!</h1>
+    <img src={logo} className='logo' alt = 'React logo'/>
+  </div>
+}
+
+export default App
