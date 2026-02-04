681d91e1fca843d3bd13f2729f54eccb1f2f02e9
Andrew Kochkin <kochkin_9@mail.ru>
2026-02-04 17:40:35 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/react-project/",
   plugins: [react()],
+  test: {
+
+    globals: true,
+
+    environment: 'jsdom',
+
+  }
 })
